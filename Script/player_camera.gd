c20ce34de3abce0d10388c781a33e162f880877e
Dilichukwu Okoli <dokoli1@sheffield.ac.uk>
2024-02-10 20:08:50 +0000

    Added limits to the camera

---
+++ Script/player_camera.gd
@@ -0,0 +1,17 @@
+extends Camera2D
+
+@onready var top_left = $Limits/TopLeft
+@onready var bottom_right = $Limits/BottomRight
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	limit_top = top_left.position.y
+	limit_left = top_left.position.x
+	limit_bottom = bottom_right.position.y
+	limit_right = bottom_right.position.x
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
