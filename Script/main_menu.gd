83d02ad881f148c7426361674a6391dabdeda116
mushroomhater07 <admin@shalev.eu.org>
2024-02-10 20:31:09 +0000

    hi

--- Script/main_menu.gd
+++ Script/main_menu.gd
@@ -21,3 +21,7 @@ func _on_texture_button_button_up():
 
 func _on_texture_button_4_button_up():
 	pass # Replace with function body.
+
+
+func _on_texture_button_3_button_up():
+	pass # Replace with function body.
