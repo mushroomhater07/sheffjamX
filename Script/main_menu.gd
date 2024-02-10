2b6ad282559d10adff7e35ec2e07e4a5afbbd6db
mushroomhater07 <admin@shalev.eu.org>
2024-02-10 23:42:35 +0000

    hi

--- Script/main_menu.gd
+++ Script/main_menu.gd
@@ -16,8 +16,8 @@ func data_init():
 	var gameobjecttemplate = preload("res://Scenes/label.tscn")
 	for key in datafile.all_time_stat:
 		var gameobject1 = gameobjecttemplate.instantiate()
-		gameobject1.setlabel1(key)
-		gameobject1.setlabel1(datafile.all_time_stat[key])
+		gameobject1.setlabel1(str(key))
+		gameobject1.setlabel1(str(datafile.all_time_stat[key]))
 		$options/stat/ScrollContainer/VFlowContainer.add_child(gameobject1)
 
 	#$"idle/lablel placeholder2".add_child(gameobject1)
