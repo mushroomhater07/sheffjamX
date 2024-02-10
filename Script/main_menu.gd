94221237f50766d3e6b101c1f1d8fa107a6d2a23
mushroomhater07 <admin@shalev.eu.org>
2024-02-10 20:34:18 +0000

    hi

--- Script/main_menu.gd
+++ Script/main_menu.gd
@@ -3,7 +3,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$option.hide();
+	$option.hide();$option/mute.hide();
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_accept"):
