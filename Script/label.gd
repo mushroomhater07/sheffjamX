852a42c114be8fc2f7509a8e97d59bbd4f721796
mushroomhater07 <admin@shalev.eu.org>
2024-02-10 23:41:51 +0000

    hi

--- Script/label.gd
+++ Script/label.gd
@@ -1,14 +1,14 @@
 extends HBoxContainer
 @onready var control = $Control
-@onready var label = $Control/Label
-@onready var label_2 = $Control/Label2
+@onready var label : Label = $Control/Label
+@onready var label_2 : Label= $Control/Label2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
 	pass # Replace with function body.
 func setlabel1(text1 : String):
-	label.text = text1
+	$Control/Label.text = text1
 func setlabel2(text1 : String):
 	label_2.text = text1
 # Called every frame. 'delta' is the elapsed time since the previous frame.
