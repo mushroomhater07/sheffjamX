30459a16e8c1b0c3e9a71a8d91f398d6abe67d46
mushroomhater07 <admin@shalev.eu.org>
2024-02-10 21:51:31 +0000

    hi

--- Script/main_menu.gd
+++ Script/main_menu.gd
@@ -3,9 +3,9 @@ extends Node
 var mute : bool;
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var datafile = JSON.parse_string(FileAccess.open("res://data/userdata.json",FileAccess.READ).get_as_text())
+	var datafile :Dictionary = JSON.parse_string(FileAccess.open("res://data/userdata.json",FileAccess.READ).get_as_text())
 	#var datafile = JSON.parse_string(FileAccess.get_file_as_string("res://data/userdata.json"))
-	print(datafile)
+	print(datafile.itemData["setting"])
 	$options.hide();$options/mute.hide();
 
 #func _unhandled_input(event):
