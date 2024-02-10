7f3ee3bc50a353ffbadac568921d95cb581071b1
Dilichukwu Okoli <dokoli1@sheffield.ac.uk>
2024-02-10 18:01:37 +0000

    Added main menu

--- Script/game_over.gd
+++ Script/game_over.gd
@@ -3,4 +3,4 @@ extends Node
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_accept"):
-		get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+		get_tree().change_scene_to_file("res://Scenes/control.tscn")
