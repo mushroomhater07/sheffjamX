235289f83338bcc762c3c648bbe1a8924323aea8
mushroomhater07 <admin@shalev.eu.org>
2024-02-11 00:26:24 +0000

    hi

--- Script/label.gd
+++ Script/label.gd
@@ -8,7 +8,7 @@ func _ready():
 	
 	pass # Replace with function body.
 func setlabel1(text1):
-	get_child(0).get_child(0).set_text(str(text1))
+	get_child(0).set_text(str(text1))
 
 func setlabel2(text1):
-	get_child(0).get_child(1).set_text(str(text1))
+	get_child(1).set_text(str(text1))
