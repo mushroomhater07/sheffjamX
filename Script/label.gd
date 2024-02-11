b4b67ce172431350535fb3554cdc12c67b4b62a2
mushroomhater07 <admin@shalev.eu.org>
2024-02-11 00:07:27 +0000

    hi

--- Script/label.gd
+++ Script/label.gd
@@ -1,14 +1,14 @@
 extends HBoxContainer
 @onready var control = $Control
-@onready var label_1 : Label = $Control/Label
-@onready var label_2 : Label= $Control/Label2
+var label_1 : Label 
+var label_2 : Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
 	pass # Replace with function body.
 func setlabel1(text1):
-	print(control)
+	label_1 = get_child(0).get_child(0)
 	control.get_child(0).set_text(str(text1))
 
 #func setlabel2(text1 : String):
