2d03871371428c9978709bf8dd2bf7d1b6f443c6
mushroomhater07 <admin@shalev.eu.org>
2024-02-11 03:11:51 +0000

    hi

--- Script/painting.gd
+++ Script/painting.gd
@@ -29,8 +29,8 @@ func _http_request_completed(result, response_code, headers, body):
 	if error != OK:
 		push_error("Couldn't load the image.")
 
-	var texture = ImageTexture.create_from_image(image)
-	get_child(0).set_texture(texture)
+	var texture = 
+	get_child(0).set_texture(ImageTexture.create_from_image(image))
 
 func _on_area_2d_area_entered(area):
 
