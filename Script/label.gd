32c330838cfc655da20a447b9b7e2748b28bf6e6
mushroomhater07 <admin@shalev.eu.org>
2024-02-11 00:03:17 +0000

    hi

--- Script/label.gd
+++ Script/label.gd
@@ -7,8 +7,9 @@ extends HBoxContainer
 func _ready():
 	
 	pass # Replace with function body.
-func setlabel1(text1 : String = ""):
+func setlabel1(text1):
 	label_1.set_text(text1)
+
 #func setlabel2(text1 : String):
 	#label_2.text = text1
 # Called every frame. 'delta' is the elapsed time since the previous frame.
