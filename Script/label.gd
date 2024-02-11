c051b6077b4b1f7f52bbad0409cf3ba53621f254
mushroomhater07 <admin@shalev.eu.org>
2024-02-11 00:01:08 +0000

    hi

--- Script/label.gd
+++ Script/label.gd
@@ -7,7 +7,7 @@ extends HBoxContainer
 func _ready():
 	
 	pass # Replace with function body.
-func setlabel1(text1):
+func setlabel1(text1 : String = ""):
 	label_1.set_text(text1)
 #func setlabel2(text1 : String):
 	#label_2.text = text1
